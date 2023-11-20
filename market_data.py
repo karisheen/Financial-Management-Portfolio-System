44d8c1b4b1bb9e12cdd99363b2ee447bc5215d89
karisheen <dakariantoine@gmail.com>
2023-11-19 23:57:06 -0600

    Add market data insertion functionality

---
+++ market_data.py
@@ -0,0 +1,58 @@
+import yfinance as yf
+import mysql.connector
+from mysql.connector import Error
+
+# Replace the values below with your MySQL database connection info
+host_name = '127.0.0.1'
+db_name = 'FMPS'
+user_name = 'root'
+user_password = 'football25'
+
+# Function to insert data into the database
+def insert_market_data(ticker_symbol, data_frame):
+    try:
+        connection = mysql.connector.connect(
+            host='127.0.0.1',
+            database='FMPS',
+            user='root',
+            password='football25'
+        )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            
+            # Get the InstrumentID that corresponds to the ticker_symbol
+            instrument_id = get_instrument_id(cursor, ticker_symbol)
+            
+            for row in data_frame.itertuples():
+                insert_query = """
+                    INSERT INTO `MARKET_DATA` (InstrumentID, Date, OpenPrice, ClosePrice, High, Low, Volume)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """
+                cursor.execute(insert_query, (instrument_id, row.Index, row.Open, row.Close, row.High, row.Low, row.Volume))
+            
+            connection.commit()
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def get_instrument_id(cursor, ticker_symbol):
+    cursor.execute("SELECT InstrumentID FROM FINANCIAL_INSTRUMENT WHERE TickerSymbol = %s", (ticker_symbol,))
+    result = cursor.fetchone()
+    return result[0] if result else None
+
+# List of top 25 stocks from the S&P 500
+stocks = ['AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'TSLA', 'GOOG',
+          'BRK-B', 'META', 'UNH', 'XOM', 'LLY', 'JPM', 'JNJ', 'V',
+          'PG', 'MA', 'AVGO', 'HD', 'CVX', 'MRK', 'ABBV', 'COST',
+          'PEP', 'ADBE']
+
+# Loop over the list of stocks
+for stock in stocks:
+    # Fetch market data
+    data_frame = yf.download(stock, start='2020-01-01', end='2020-12-31')
+
+    # Insert market data into the database
+    insert_market_data(stock, data_frame)
